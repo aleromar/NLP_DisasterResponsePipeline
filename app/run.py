6b6ca49306e0dc45b0663f5d2518bd10a0c8c4d2
Alejandro Martinez <alejandro.martinez@non.keysight.com>
2019-08-23 10:36:04 +0100

    FEAT: Adding webapp

---
+++ app/run.py
@@ -0,0 +1,170 @@
+import json
+import plotly
+import pandas as pd
+import numpy as np
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+from flask import Flask
+from flask import render_template, request, jsonify
+from plotly.graph_objs import Bar
+from sklearn.externals import joblib
+from sqlalchemy import create_engine
+
+from sklearn.base import BaseEstimator, TransformerMixin, ClassifierMixin
+
+import pickle
+
+app = Flask(__name__)
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            #print(len(pos_tags))
+            if len(pos_tags) > 0:
+                first_word, first_tag = pos_tags[0]
+                if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                    return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+    
+def tokenize(text):
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+# load data
+database_filepath = r'./data/DisasterResponse.db'
+engine = create_engine('sqlite:///'+database_filepath)
+df = pd.read_sql_table('DisasterResponseTable',engine)
+#engine = create_engine('sqlite:///../data/DisasterResponse.db')
+#df = pd.read_sql_table('DisasterResponseTable', engine)
+
+# load model
+#model = pickle.load(open('./models/classifier.pkl', 'rb'))
+model = joblib.load("./models/classifier.pkl")
+
+
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():
+    
+    # extract data needed for visuals
+    # TODO: Below is an example - modify to extract data for your own visuals
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+    
+    newdf = df.sum(axis=0)[2:].sort_values(ascending=False)[0:10]
+    mostPopularCategoriesCount = newdf.values
+    mostPopularCategoriesTags = newdf.index
+    
+    freq, tags = np.histogram(df.drop(columns=['message','genre']).sum(axis=1).values,27)
+    tags = [tag + 1 for tag in tags][0:-1]
+    
+    # create visuals
+    # TODO: Below is an example - modify to create your own visuals
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=genre_names,
+                    y=genre_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=mostPopularCategoriesTags,
+                    y=mostPopularCategoriesCount
+                )
+            ],
+
+            'layout': {
+                'title': 'Most advertised categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Category"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=tags,
+                    y=freq
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution on how a single message applies to a number of categories',
+                'yaxis': {
+                    'title': "Number of messages"
+                },
+                'xaxis': {
+                    'title': "Number of Categories"
+                }
+            }
+        },
+    ]
+    
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '') 
+
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html Please see that file. 
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
+
+
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
+
+
+if __name__ == '__main__':
+    main()
